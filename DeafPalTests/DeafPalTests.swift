24f0ece59c9867e9685b7605b0f22306933b9e2c
Kurniakun17 <kurniakharisma17@gmail.com>
2024-10-25 16:28:59 +0700

    Initial Commit

---
+++ DeafPalTests/DeafPalTests.swift
@@ -0,0 +1,17 @@
+//
+//  DeafPalTests.swift
+//  DeafPalTests
+//
+//  Created by Kurnia Kharisma Agung Samiadjie on 25/10/24.
+//
+
+import Testing
+@testable import DeafPal
+
+struct DeafPalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
